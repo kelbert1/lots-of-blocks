714ec79e0b4c226862a75ba82491e497ee3a1cec
jmurphy61 <jones.murphy1994@gmail.com>
2017-10-17 20:28:04 -0400

    Node app entry point

--- index.js
+++ index.js
@@ -1,47 +1,33 @@
-/**
- * Creates the server
- *
- * @author: Jones Murphy
- */
-var express = require('express');
-var path = require('path');
-var ejs = require('ejs');
+var http = require('http');
+var fs = require('fs');
 
-var app = express();
-var CURR_DIR = __dirname;
-
-app.set('views', path.join(CURR_DIR, 'public'));
-app.set('view engine', 'html');
-
-app.use(express.static(path.join(CURR_DIR, 'public')));
-
-// Make sure all request return CORS headers
-app.use(function (req, res, next) {
-    var origin = req.get('origin');
-    if (!origin || origin === 'undefined' || origin.length == 0) {
-        origin = req.get('host');
+http.createServer(function(req, res) {
+    var url = req.url;
+    switch(url) {
+        case '/':
+            getStaticFileContent(res, 'public/index.html', 'text/html');
+            break;
+        case '/about':
+            // TODO
+            break;
+        case '/status':
+            // TODO
+            break;
+        default:
+            res.writeHead(404, {'Content-Type':'text/plain'});
+            res.end('404 - Page Not Found');
     }
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, token, newpassword');
+}).listen(8080);
 
-    next();
-});
-
-//Route to home page
-app.get('/', function(req, res) {
-    res.render(index);
-});
-
-// Node App Port
-var port = 443;
-
-createServer(port);
-
-/**
- * Creates the server.
- */
-function createServer(config_port) {
-    app.listen(config_port);
-    console.log("Server running on %s mode. Listening on port %d", app.settings.env, port);
+function getStaticFileContent(res, filePath, contentType) {
+    fs.readFile(filePath, function(error, data) {
+        if (error) {
+            res.writeHead(500, {'Content-Type':'text/plain'});
+            res.end('500 - Internal Server Error');
+        }
+        if (data) {
+            res.writeHead(200, {'Content-Type':'text/html'});
+            res.end(data);
+        }
+    })
 }
